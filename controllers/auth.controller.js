23bc1e9016433ff26a1ec122c1baea369a299ddc
adesiyanoladipo <adesiyanoladipo1@gmail.com>
2023-03-05 12:46:28 +0100

    feat: added user login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
-const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const seeders = require('../config/seeders.config');
 const jwt = require('jsonwebtoken')
 const ApiError  = require('../error/ApiError')
+const knex = require('../config/database')
 
 const { signup } = require('../utils/validator')
 const { createUser } = require('../repositories/user.repository')
@@ -15,7 +16,7 @@ module.exports.register = async function(req, res, next) {
       username = username.trim()
       email = email.trim()
       password = password.trim()
-      const newPassword = await bcryptjs.hash(password, 10)
+      const newPassword = await bcrypt.hash(password, 10)
       const { errors, valid } = signup(name, username, email, password);  
       if(!valid){
         next(ApiError.badUserRequest(errors.error))
@@ -32,6 +33,41 @@ module.exports.register = async function(req, res, next) {
         )
       }   
     }
+    catch(err){
+      next({err})
+    }
+}
+
+module.exports.login = async function(req, res, next) {
+    try{
+        let { email, password } = req.body
+        email = email.trim()
+        password = password.trim()
+
+        const user = await knex('users').where('email', email).first();
+        if(!user){
+            return res.status(400).send({
+                "success": false,
+                "message": "User with this email not found"
+            })
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if(match == true){
+            const accessToken = jwt.sign({_id: user.id}, seeders[NodeEnv].jwt_access_token_secret, { expiresIn: seeders[NodeEnv].jwt_expiry_time  })
+            const user_wallet = await knex('wallets').where('user_id', user.id).first();
+            return res.status(200).send({
+                "success": true,
+                "token": accessToken,
+                "data": {
+                    user, user_wallet
+                }
+            })
+        }
+        return res.status(400).send({
+            "success": false,
+            "message": "Incorrect password"
+        })
+    }   
     catch(err){
       next({err})
     }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,5 +4,6 @@ const { userExists } = require('../utils/middleware/userExistsByEmail')
 const router = express.Router();
 
 router.post('/register', userExists, controller.register)
+router.post('/login', controller.login)
 
 module.exports = router;
