e0724f180b9cd759f89c2760dd34d0d1cbdbfd07
adesiyanoladipo <adesiyanoladipo1@gmail.com>
2023-03-19 18:34:03 +0100

    feat: added tests for login

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -44,4 +44,34 @@ describe(`${seeders[NodeEnv].server_name} - Account Integration tests`, () => {
             });
         });
     });
+    describe("Post /api/v1/auth/login", function() {
+        this.timeout(5000);
+        it("It should Login a user", (done) => {
+            const user = {
+                email: '0iNdM7ow@gmail.com',
+                password: 'test1234'
+            }
+            chai.request(app)
+                .post("/api/v1/auth/login")
+                .send(user)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');   
+                done();
+            });
+        });
+        it("It should NOT Login a user that failed validation", (done) => {
+            const user = {
+                email: '0iNdM7ow@gmail.com',
+                password: 'test1234.'
+              }
+            chai.request(app)
+                .post("/api/v1/auth/login")
+                .send(user)
+                .end((err, response) => {
+                    response.should.have.status(409);
+                done();
+            });
+        });
+    });
 });
